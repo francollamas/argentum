71d3f5f882ffddc327c70b45eb85400c92b538e5
Franco Llamas <48653836+francollamas@users.noreply.github.com>
2024-08-14 23:24:59 -0300

    Added CI github action (#1)

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './components/App'
 import { Provider } from 'react-redux'
 import { PersistGate } from 'redux-persist/integration/react'
+import App from './components/App'
 import { persistor, store } from './store/store.ts'
 import './index.css'
 
