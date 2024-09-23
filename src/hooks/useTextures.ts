851dc0f1af4f3988968149d5558c760d31c9ffb0
Franco Llamas <francollamas077@gmail.com>
2024-09-23 01:13:48 -0300

    Changes

--- src/hooks/useTextures.ts
+++ src/hooks/useTextures.ts
@@ -1,20 +1,21 @@
 import {useCallback, useEffect, useState} from "react";
-import {Assets, Spritesheet, SpritesheetData, Texture} from "pixi.js";
+import {Assets, Spritesheet, Texture} from "pixi.js";
+import {textureData as textureDataList} from "../assets.ts";
 
-const loadJson = async <T>(fileName: string): Promise<T> => {
-    const file = await import(`../assets/textures/${fileName}.json`);
-    return file.default
-}
-
-const loadImage = async (fileName: string): Promise<HTMLImageElement> => {
-    const imageUrl = new URL(`../assets/textures/${fileName}`, import.meta.url).href
-    return new Promise((resolve, reject) => {
-        const img = new Image()
-        img.src = imageUrl
-        img.onload = () => resolve(img)
-        img.onerror = (error) => reject(error)
-    });
-};
+// const loadJson = async <T>(fileName: string): Promise<T> => {
+//     const file = await import(`../assets/textures/${fileName}.json`);
+//     return file.default
+// }
+//
+// const loadImage = async (fileName: string): Promise<HTMLImageElement> => {
+//     const imageUrl = new URL(`../assets/textures/${fileName}`, import.meta.url).href
+//     return new Promise((resolve, reject) => {
+//         const img = new Image()
+//         img.src = imageUrl
+//         img.onload = () => resolve(img)
+//         img.onerror = (error) => reject(error)
+//     });
+// };
 
 export const useTextures = () => {
     const [textureRecord, setTextureRecord] = useState<Record<string, Texture>>({})
@@ -23,17 +24,18 @@ export const useTextures = () => {
 
     useEffect(() => { // TODO F: sacar afuera y llamar desde el arranque para que no se vuelva a ejecutar!!! (ver de guardar en redux, o algun contexto de Pixi)
         (async () => {
-            const textureList = await loadJson<string[]>("textures")
+
+            //const textureList = await loadJson<string[]>("textures")
             const parsedSpritesheetList: Record<string, Texture>[] = []
 
-            for (const textureFileName of textureList) {
-                const jsonFile = await loadJson<SpritesheetData>(textureFileName)
-                if (!jsonFile.meta?.image) continue; // TODO F deberiamos manejar este error?
+            for (const textureData of textureDataList) {
 
-                const imageFile = await loadImage(jsonFile.meta?.image)
-                const imageTexture = await Assets.load(imageFile)
+                //if (!textureData.json.meta?.image) continue; // TODO F deberiamos manejar este error?
 
-                const spritesheet = new Spritesheet(imageTexture, jsonFile)
+                //const imageFile = await loadImage(jsonFile.meta?.image)
+                const imageTexture = await Assets.load(textureData.png)
+
+                const spritesheet = new Spritesheet(imageTexture, textureData.json)
                 const parsedSpritesheet = await spritesheet.parse()
                 parsedSpritesheetList.push(parsedSpritesheet)
 
