d89958d1fe7a57c3d52fe4772603bdc17ae024db
Franco Llamas <francollamas077@gmail.com>
2025-11-18 22:51:38 -0300

    Enhanced UI size

--- scripts/assets/generate-ui.ts
+++ scripts/assets/generate-ui.ts
@@ -4,11 +4,41 @@ import path from 'node:path'
 import { fileURLToPath } from 'node:url'
 import util from 'node:util'
 import sharp from 'sharp'
+import type { SpritesheetData } from 'pixi.js'
 
 // Get the current directory path
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = path.dirname(__filename)
 const __assetspath = path.join(__dirname, '../../src/assets')
+
+async function loadJSON<T>(filePath: string): Promise<T> {
+    const data = await fs.promises.readFile(filePath, 'utf-8')
+    return JSON.parse(data)
+}
+
+async function generateSpritesheetFile(outputPath: string) {
+    const files = await fs.promises.readdir(outputPath)
+    const jsonFiles = files.filter(
+        (file) => file.endsWith('.json') && file !== 'spritesheets.json',
+    )
+
+    const entries: string[] = []
+    for (const jsonFile of jsonFiles) {
+        const jsonPath = path.join(outputPath, jsonFile)
+        const jsonData = await loadJSON<SpritesheetData>(jsonPath)
+        const keys = Object.keys(jsonData.frames)
+
+        const baseName = path.basename(jsonPath, '.json').replace('-', '')
+        for (const key of keys) {
+            entries.push(`  "${key}": "${baseName}"`)
+        }
+    }
+
+    const texMap = '{\n' + entries.join(',\n') + '\n}\n'
+
+    const spritesheetsFilePath = path.join(outputPath, 'spritesheets.json')
+    await fs.promises.writeFile(spritesheetsFilePath, texMap)
+}
 
 async function generateUITextures() {
     const texPackerPath = path.join(__dirname, '../../tools/texpacker')
@@ -28,7 +58,7 @@ async function generateUITextures() {
             const outputFile = svgFile.replace('.svg', '.png')
             const outputFilePath = path.join(pngTempPath, outputFile)
 
-            await sharp(inputPath).png().toFile(outputFilePath)
+            await sharp(inputPath, { density: 72 * 2 }).png().toFile(outputFilePath)
         }
 
         const projectPath = path.join(texPackerPath, 'ui.ftpp')
@@ -42,6 +72,8 @@ async function generateUITextures() {
         await execPromise(
             `npx free-tex-packer-cli --project ${tempProjectPath} --output ${outputPath}`,
         )
+
+        await generateSpritesheetFile(outputPath)
     } finally {
         await fs.promises.unlink(tempProjectPath).catch(() => {})
         await fs.promises.rm(pngTempPath, { recursive: true, force: true })
